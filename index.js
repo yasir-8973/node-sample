8d2f97b327ffb6761188b841ebd3566d7e363db7
yasir-8973 <mohammedyasir1771@gmail.com>
2022-12-12 21:28:22 +0530

    as

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 app.post('/get', Text.Text);
