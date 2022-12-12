47274ff9a2dc0102f6dd548c550d974cfdaf93bd
yasir-8973 <mohammedyasir1771@gmail.com>
2022-12-12 21:06:26 +0530

    as

--- index.js
+++ index.js
@@ -21,21 +21,5 @@ app.get('/', (req,res) => {
 app.post('/get', Text.Text);
 
 app.listen(port, () =>{
-    console.log("app running in port "+port);
+  console.log("app running in port "+port);
 });
-
-const db = require("./app/models");
-db.mongoose
-.connect(db.url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log("Connected to the database!");
-})
-.catch(err => {
-  console.log("Cannot connect to the database!", err);
-  process.exit();
-});
-
-require("./app/routes/turorial.routes")(app);
